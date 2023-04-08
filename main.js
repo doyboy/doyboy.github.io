a44b75940ea7ada9fd6baa5d97a35b0ca14ac099
doyboy33 <doyboy33@gmail.com>
2023-04-07 20:48:05 -0400

    fixed css issues & added sidebar functionality

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const API_URL = `https://e621.net/posts.json?limit=1&tags=score%3A%3E1000+order%
 const searchForm = document.querySelector('.search-form');
 const searchInput = document.querySelector('#search-input');
 const gallery = document.querySelector('.gallery');
+const sidebar = document.querySelector('.sidebar');
+const sidebarArray = [];
 
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -12,36 +14,86 @@ searchForm.addEventListener('submit', (event) => {
     newSearchTerm = newSearchTerm.replace(/ /g, "+");
     console.log(`search term after change: ${newSearchTerm}`);
     const searchUrl = `https://e621.net/posts.json?limit=1&tags=-cub+-loli+-shota+-young+-female+-feral+-intersex+-diaper+-scat+-watersports+-urine+-feces+-gore+${newSearchTerm}`;
-    const testElement = document.createElement('div');
-    testElement.innerHTML = searchUrl;
     fetchImages(searchUrl);
 });
 
-// fetchImages(API_URL);
+function showImage(data) {
+    gallery.innerHTML = '';
+    console.log(data);
+    const sourceUrlContainer = document.createElement('a');
+    const sourceUrl = `https://e621.net/posts/${data.posts[0].id}`;
+    sourceUrlContainer.href = sourceUrl;
+    sourceUrlContainer.target = "_blank";
+    sourceUrlContainer.innerHTML = `Source: ${sourceUrl}`;
+    gallery.appendChild(sourceUrlContainer);
+    const fileUrl = data.posts[0].file.url;
+    const arr = fileUrl.split(".");
+    if (arr[arr.length - 1] === "webm") {
+        console.log("this is a video");
+        const vid = document.createElement('video');
+        vid.src = fileUrl;
+        vid.controls = true;
+        vid.autoplay = true;
+        vid.muted = true;
+        vid.loop = true;
+        gallery.appendChild(vid);
+    } else {
+        const img = document.createElement('img');
+        img.src = data.posts[0].file.url;
+        gallery.appendChild(img);
+    }
+}
+
+function sidebarFunctionality(data) {
+    const previewImg = document.createElement('img');
+    previewImg.src = data.posts[0].preview.url;
+    sidebar.appendChild(previewImg);
+    sidebarArray.push(previewImg);
+    sidebarArray[sidebarArray.length - 1].addEventListener("click", () => {
+        showImage(data);
+    });
+}
 
 function fetchImages(url) {
     gallery.innerHTML = '';
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
-            const fileUrl = data.posts[0].file.url;
-            const arr = fileUrl.split(".");
-            if (arr[arr.length - 1] === "webm") {
-                console.log("this is a video");
-                const vid = document.createElement('video');
-                vid.src = fileUrl;
-                vid.controls = true;
-                vid.autoplay = true;
-                vid.muted = true;
-                vid.loop = true;
-                gallery.appendChild(vid);
-            } else {
-                const img = document.createElement('img');
-                img.src = data.posts[0].file.url;
-                gallery.appendChild(img);
-            }
-
+            showImage(data);
+            sidebarFunctionality(data);
+            // console.log(data);
+            // const testElement = document.createElement('a');
+            // const sourceUrl = `https://e621.net/posts/${data.posts[0].id}`;
+            // testElement.href = sourceUrl;
+            // testElement.innerHTML = `Source: ${sourceUrl}`;
+            // gallery.appendChild(testElement);
+            // const fileUrl = data.posts[0].file.url;
+            // const arr = fileUrl.split(".");
+            // if (arr[arr.length - 1] === "webm") {
+            //     console.log("this is a video");
+            //     const vid = document.createElement('video');
+            //     vid.src = fileUrl;
+            //     vid.controls = true;
+            //     vid.autoplay = true;
+            //     vid.muted = true;
+            //     vid.loop = true;
+            //     gallery.appendChild(vid);
+            // } else {
+            //     const img = document.createElement('img');
+            //     img.src = data.posts[0].file.url;
+            //     gallery.appendChild(img);
+            // }
+            // const previewImg = document.createElement('img');
+            // previewImg.src = data.posts[0].preview.url;
+            // sidebar.appendChild(previewImg);
+            // sidebarArray.push(previewImg);
+            // sidebarArray[sidebarArray.length - 1].addEventListener("click", (e) => {
+            //     gallery.innerHTML = '';
+            //     const img = document.createElement('img');
+            //     img.src = data.posts[0].file.url;
+            //     gallery.appendChild(img);
+            //     // console.log(e.target);
+            // });
         })
         .catch((error) => console.log(error));
 }
